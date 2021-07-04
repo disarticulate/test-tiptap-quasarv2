41ee39aea1e86e2b40b698848be8f45f4032496a
cy <eric@lorefolk.com>
2021-07-04 11:07:37 -0500

    base test

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <IndexPage/>
+  </q-layout>
+</template>
+
+<script>
+import IndexPage from '../pages/Index'
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    IndexPage
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
